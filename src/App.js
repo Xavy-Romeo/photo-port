1ffa30fe90fa6d3aa62fe92ae738974fecc33c8a
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-06-25 16:22:39 -0700

    conditionally rendered contact form when clicked in navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React, {useState} from 'react';
-// import logo from './logo.svg';
-import './App.css';
-
-import About from './components/About';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
+import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
@@ -19,21 +16,31 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
-  
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       >
       </Nav>
       <main>
-        <div>
-          <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        {!contactSelected 
+          ? (
+              <>
+                <Gallery currentCategory={currentCategory}></Gallery>
+                <About></About>
+              </>
+            ) 
+          : (
+            <ContactForm></ContactForm>
+            )
+        }
       </main>
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,6 +6,8 @@ function Nav(props) {
         categories = [],
         setCurrentCategory,
         currentCategory,
+        contactSelected,
+        setContactSelected
     } = props;
 
     useEffect(() => {
@@ -23,13 +25,13 @@ function Nav(props) {
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a data-testid="about" href="#about">
+                        <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
                             About me
                         </a>
                     </li>
 
-                    <li>
-                        <span>Contact</span>
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                        <span onClick={() => setContactSelected(true)}>Contact</span>
                     </li>
 
                     {categories.map((category) => (
@@ -40,7 +42,8 @@ function Nav(props) {
                         >
                         
                             <span onClick={() => {
-                                setCurrentCategory(category)
+                                setCurrentCategory(category);
+                                setContactSelected(false);
                             }}>
                                 {capitalizeFirstLetter(category.name)}
                             </span>
